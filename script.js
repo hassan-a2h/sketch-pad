4c5337f99c5eb6bcfdcc22e98c5100b396791c4f
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-05-22 19:40:22 +0500

    Print grid of user defined size. Clear grid each time a new size is provided.
    * Remove background color of parent div
    * Refactor js file to have the grid creation inside a function

--- script.js
+++ script.js
@@ -9,12 +9,44 @@ inputButton.addEventListener('click', () => {
       continue;
     }
     else {
+      if (canvasSize > 100 || canvasSize < 2) {
+        alert('Size limit exceeded (1 < size < 101)');
+        continue;
+      }
       break;
     }
   } while (true);
+
+  clearGrid();
+  displayGrid();
 });
 
-const container = document.getElementsByClassName('container');
-for (let i = 0; i < canvasSize * canvasSize; i++) {
+// Driver Code
+displayGrid();
 
+// Functions
+function displayGrid() {
+  let boxSize = Math.floor(800 / canvasSize),
+    spacing = Math.floor(boxSize / 8);
+  boxSize -= spacing;
+
+  const canvas = document.querySelector('.canvas');
+  canvas.style.cssText += `display: flex; flex-wrap: wrap; gap: ${spacing}px; `;
+
+  for (let i = 0; i < canvasSize * canvasSize; i++) {
+    const box = document.createElement('div');
+    box.classList.add('grid-element');
+    box.setAttribute('style', `height: ${boxSize}px; width: ${boxSize}px;\
+    color: white; box-shadow: 0px 0px 2px 0.5px black`);
+    canvas.appendChild(box);
+  }
+}
+
+function clearGrid() {
+  const gridElements = document.querySelectorAll('.grid-element');
+  if (gridElements) {
+    gridElements.forEach((gridElement) => {
+      gridElement.remove();
+    });
+  }
 }
